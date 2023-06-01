7483bc7113e79d24f03115ecc70b1f8c7375ce8d
JuandaMT <juandamt@outlook.es>
2023-06-01 11:04:06 +0200

    corregido

--- numero.js
+++ numero.js
@@ -1,11 +1,9 @@
-const numeroDado = prompt("introduzca un numero")
-parseInt(numeroDado)
-const numeroLimite = numeroDado + 50;
+let numeroDado = prompt("Introduce un numero");
+let numero = parseInt(numeroDado)
+let numeroLimite = numero + 50;
 
-console.log(numeroLimite)
-
-/* for(let i = numeroDado; i <= numeroLimite; i++){
+for(let i = numeroDado; i <= numeroLimite; i++){
     if(i%2 !== 0){
         console.log(i);
     }
-} */
+}
